86e72551d815fb9c35192d5b2a2087526f060262
Arvis Abromovics <info@vsmedio.lv>
2018-01-10 19:09:32 +0100

    Initial commit

---
+++ src/Solution1315.java
@@ -0,0 +1,100 @@
+/* 
+Том, Джерри и Спайк
+1. Создай классы Dog, Cat и Mouse.
+2. Реализуй интерфейсы в добавленных классах, учитывая что:
+- Кот(Cat) может передвигаться, может кого-то съесть и может быть сам съеден.
+- Мышь(Mouse) может передвигаться и может быть съедена.
+- Собака(Dog) может передвигаться и съесть кого-то.
+
+
+Требования:
+1. Класс Cat должен быть объявлен внутри класса Solution.
+2. Класс Dog должен быть объявлен внутри класса Solution.
+3. Класс Mouse должен быть объявлен внутри класса Solution.
+4. Кот(Cat) может передвигаться, может кого-то съесть и может быть сам съеден.
+5. Мышь(Mouse) может передвигаться и может быть съедена.
+6. Собака(Dog) может передвигаться и съесть кого-то.
+
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    //может двигаться
+    public interface Movable {
+        void move();
+    }
+
+    //может быть съеден
+    public interface Eatable {
+        void eaten();
+    }
+
+    //может кого-нибудь съесть
+    public interface Eat {
+        void eat();
+    }
+}
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    //может двигаться
+    public interface Movable {
+        void move();
+    }
+
+    //может быть съеден
+    public interface Eatable {
+        void eaten();
+    }
+
+    //может кого-нибудь съесть
+    public interface Eat {
+        void eat();
+    }
+
+    class Dog implements Movable, Eat{
+
+        @Override
+        public void move() {
+
+        }
+
+        @Override
+        public void eat() {
+
+        }
+    }
+    class Cat implements Movable, Eatable, Eat{
+
+        @Override
+        public void move() {
+
+        }
+
+        @Override
+        public void eaten() {
+
+        }
+
+        @Override
+        public void eat() {
+
+        }
+    }
+    class Mouse implements Movable, Eatable {
+
+        @Override
+        public void move() {
+
+        }
+
+        @Override
+        public void eaten() {
+
+        }
+    }
+}
